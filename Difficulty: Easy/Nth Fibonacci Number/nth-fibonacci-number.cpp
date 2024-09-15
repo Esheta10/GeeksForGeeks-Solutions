b391404ad0b0532ac14452505f3b56bbe66b2ed8
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-15 19:41:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -9,14 +9,15 @@ class Solution {
   public:
     int nthFibonacci(int n){
         // code here
-        const int MOD=1000000007;
-        if(n==1)
-            return 1;
-            
-        int a=1,b=1;
-        for(int i=3;i<=n;++i)
+        const int MOD = 1e9 + 7; // Define the modulo
+        if (n == 0) return 0;
+        
+       long long a=0;
+       long long b=1;
+       long long temp=0;
+        for(int i=2;i<=n;i++)
         {
-            int temp=(a+b)%MOD;
+            temp=(a+b)%MOD;
             a=b;
             b=temp;
         }
