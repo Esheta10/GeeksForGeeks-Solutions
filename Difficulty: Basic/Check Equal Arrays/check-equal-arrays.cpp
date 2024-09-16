fcf2a0a3f6f4cf46be46f7b767854b3114e6c0c0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-16 16:30:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check Equal Arrays/check-equal-arrays.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to check if two arrays are equal or not.
+    bool check(vector<int>& arr1, vector<int>& arr2) {
+        // code here
+        unordered_map<int,int> freq;
+        for(int num:arr1)
+            freq[num]++;
+            
+        for(int num:arr2)
+            freq[num]--;
+            
+        for(auto it:freq)
+        {
+            if(it.second!=0)return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+    while (t--) {
+        vector<int> arr1, arr2;
+        string input1, input2;
+
+        getline(cin, input1); // Read the entire line for the first array elements
+        stringstream ss1(input1);
+        int number;
+        while (ss1 >> number) {
+            arr1.push_back(number);
+        }
+
+        getline(cin, input2); // Read the entire line for the second array elements
+        stringstream ss2(input2);
+        while (ss2 >> number) {
+            arr2.push_back(number);
+        }
+
+        Solution ob;
+        cout << (ob.check(arr1, arr2) ? "true" : "false") << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
