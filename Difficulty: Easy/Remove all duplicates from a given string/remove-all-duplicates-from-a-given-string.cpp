4c420f90ecb5d53de62c11dece9429704d3a9986
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-08 02:42:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.cpp
+++ Difficulty: Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.cpp
@@ -10,23 +10,17 @@ class Solution{
 public:
 	string removeDuplicates(string str) {
 	    // code here
-     // Create an array to mark the presence of characters
-        std::vector<bool> visited(256, false);  // ASCII size, supports all characters
-        int len = str.length();
-        int index = 0;  // Pointer for the result string
-        
-        // Traverse through the input string
-        for (int i = 0; i < len; i++) {
-            // If the character is not visited, append it to result
-            if (!visited[(int)str[i]]) {
-                visited[(int)str[i]] = true;  // Mark character as visited
-                str[index++] = str[i];        // Append character
-            }
-        }
-        
-        // Resize the string to the new length (index)
-        str.resize(index);
-        return str;
+	    unordered_set<char> seen;
+	    string result;
+	    for(char ch:str)
+	    {
+	        if(seen.find(ch)==seen.end())
+	        {
+	            result.push_back(ch);
+	            seen.insert(ch);
+	        }
+	    }
+	    return result;
 	}
 };
 
@@ -42,7 +36,9 @@ int main() {
         auto ans = ob.removeDuplicates(str);
 
         cout << ans << "\n";
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
