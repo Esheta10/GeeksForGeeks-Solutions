b57b3cfe2c9d74c1f4a771438f4cbab6835ba8f5
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-20 10:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.cpp
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -13,54 +13,31 @@ class Solution {
     int majorityElement(vector<int>& arr) {
 
         // your code here
-        int n=arr.size();
+        int n = arr.size();
+        int count = 0;
+        int maj = 0;
         
-        if (n == 1) return arr[0];
-        if (n == 2) return arr[0] == arr[1] ? arr[0] : -1;
-        
-        int maj1=INT_MIN;
-        int count1=0;
-        
-        int maj2=INT_MIN;
-        int count2=0;
         for(int i=0;i<n;i++)
         {
-            if(arr[i]==maj1)
+            if(count==0)
             {
-                count1++;
+                maj = arr[i];
+                count = 1;
             }
-            else if(arr[i]==maj2)
-            {
-                count2++;
-            }
-            else if(count1==0)
-            {
-                maj1=arr[i];
-                count1=1;
-            }
-            else if(count2==0)
-            {
-                maj2=arr[i];
-                count2=1;
-            }
+            else if(arr[i]==maj)
+                count++;
             else
-            {
-                count1--;
-                count2--;
-            }
+                count--;
         }
-        
-        int freq1=0,freq2=0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == maj1) freq1++;
-            else if (arr[i] == maj2) freq2++;
-        }
-        if(freq1>n/2)
-            return maj1;
-        if(freq2>n/2)
-            return maj2;
-       
-        
+       int freq = 0;
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i]==maj)
+            freq++;
+       }
+       if(freq > floor(n/2))
+            return maj;
+    
         return -1;
     }
 };
