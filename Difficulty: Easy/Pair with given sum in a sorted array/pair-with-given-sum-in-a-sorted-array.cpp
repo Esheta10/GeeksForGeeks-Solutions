d6ff694b7510d7a5d9a62c6848a05fc563d9cbbe
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-01-07 23:41:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.cpp
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.cpp
@@ -7,26 +7,42 @@ using namespace std;
 
 class Solution {
   public:
-    int countPair(int k, vector<int> &arr) {
+    int countPairs(vector<int> &arr, int target) {
         // Complete the function
-        int i=0;
-        int j=arr.size()-1;
-        int count=0;
-        while(i<j)
-        {
-            int sum=arr[i]+arr[j];
-            if(sum<k)
-                i++;
-            else if(sum>k)
-                j--;
-            else
-            {
-               count++;
-               i++;
-               j--;
+        int n = arr.size();
+    int left = 0, right = n - 1;
+    int count = 0;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            // Count duplicates if any
+            if (arr[left] == arr[right]) {
+                int freq = right - left + 1;
+                count += (freq * (freq - 1)) / 2; // nC2 for pairs
+                break;
+            } else {
+                int leftCount = 1, rightCount = 1;
+                while (left + 1 < right && arr[left] == arr[left + 1]) {
+                    leftCount++;
+                    left++;
+                }
+                while (right - 1 > left && arr[right] == arr[right - 1]) {
+                    rightCount++;
+                    right--;
+                }
+                count += leftCount * rightCount;
+                left++;
+                right--;
             }
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
-        return count;
+    }
+
+    return count;
     }
 };
 
@@ -35,13 +51,11 @@ int main() {
 
     int t;
     cin >> t;
-
+    cin.ignore();
     while (t--) {
         vector<int> arr;
-        int k;
-        cin >> k;
+        int target;
         string input;
-        getline(cin, input);
         getline(cin, input);
 
         stringstream ss(input);
@@ -49,9 +63,11 @@ int main() {
         while (ss >> number) {
             arr.push_back(number);
         }
-
+        cin >> target;
+        cin.ignore();
         Solution obj;
-        cout << obj.countPair(k, arr) << endl;
+        cout << obj.countPairs(arr, target) << endl;
+        cout << "~\n";
     }
 
     return 0;
