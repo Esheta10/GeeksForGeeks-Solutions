940dbab31d68cd57ef9ab46880b1539f1f7d6739
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-29 14:03:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int> freq(n+1,0);
+        
+        int missing = -1, duplicate = -1;
+        
+        for(int num : arr){
+            freq[num]++;
+        }
+        
+        for(int i=1;i<=n;i++){
+            
+            if(freq[i] == 0)
+                missing = i;
+            if(freq[i] == 2)
+                duplicate = i;
+                
+        }
+        return {duplicate, missing};
+    }
+};
