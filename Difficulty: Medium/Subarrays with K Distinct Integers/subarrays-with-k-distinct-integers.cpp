6edb4ff217da2d278e1540cf4d40d22135f56b99
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-04 19:37:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with K Distinct Integers/subarrays-with-k-distinct-integers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    int slidingWindow(vector<int>& arr, int k){
+        
+        unordered_map<int,int> mp;
+        int n = arr.size();
+        
+        int i=0;
+        int j=0;
+        
+        int count=0;
+        
+        while(j<n){
+            
+            mp[arr[j]]++;
+            
+                while(mp.size() > k){
+                    
+                    mp[arr[i]]--;
+                    
+                    if(mp[arr[i]] == 0)
+                    {
+                        mp.erase(arr[i]);
+                    }
+                    i++;
+                }
+            
+            count += (j-i+1);
+            j++;
+        }
+        return count;
+    }
+    int exactlyK(vector<int> &arr, int k) {
+        // code here
+        return slidingWindow(arr, k) - slidingWindow(arr, k-1);
+    }
+};
