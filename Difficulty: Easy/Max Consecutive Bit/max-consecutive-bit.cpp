1489decaf9e4a27170ba6e7348a18e724d3d126b
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-05 21:20:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.cpp
+++ Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.cpp
@@ -2,23 +2,19 @@ class Solution {
   public:
     int maxConsecBits(vector<int> &arr) {
         // code here
+        int n = arr.size();
+        int currentCount = 1;
+        int maxCount = 1;
         
-        int max0 = 0, max1 = 0;
-        int count1 = 0, count0 = 0;
-        
-        for(int num : arr){
+        for(int i=1;i<n;i++){
             
-            if(num == 1){
-                count1++;
-                max1 = max(max1,count1);
-                count0 = 0; // reset zero streak
+            if(arr[i] == arr[i-1]){
+                currentCount++;
+                maxCount = max(currentCount, maxCount);
             } else {
-               count0++;
-               max0 = max(max0, count0);
-               count1 =  0; // reset 1 streak
+                currentCount = 1; // reset the streak
             }
         }
-        
-        return max(max0,max1);
+        return maxCount;
     }
 };
