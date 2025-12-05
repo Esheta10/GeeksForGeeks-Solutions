2cb8bc878e26e1ba80c3cf70020edc96cb88d85e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-05 20:06:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.cpp
+++ Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.cpp
@@ -7,13 +7,14 @@ class Solution {
         for(int i=0;i<n;i+=k){
             
             int left = i;
-            int right = min(i+k-1, n-1);
+            int right = min(i+k-1,n-1);
             
-            while(left < right){
+            while(left<right){
                 swap(arr[left],arr[right]);
                 left++;
                 right--;
             }
         }
+        
     }
 };
