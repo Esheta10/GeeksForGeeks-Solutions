823a07dd580ba18b741c60477c308e2cd01c6d4a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-06 00:06:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Adding One/adding-one.cpp
+++ Difficulty: Easy/Adding One/adding-one.cpp
@@ -3,19 +3,17 @@ class Solution {
     vector<int> addOne(vector<int>& arr) {
         // code here
         int n = arr.size();
-        int carry = 1;
         
-        for(int i=n-1;i>=0;i--){
-            int sum = arr[i] + carry;
+        for(int i = n-1; i>=0; i--){
             
-            arr[i] = sum % 10;
-            carry = sum / 10;
+            if(arr[i] < 9){
+                 arr[i]++;
+                 return arr;
+            }
+            arr[i] = 0;
         }
+        arr.insert(arr.begin(), 1);
         
-        if(carry)
-            arr.insert(arr.begin(),1);
-            
-            
         return arr;
     }
 };
