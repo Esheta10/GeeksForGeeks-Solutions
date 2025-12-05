f2497a851f8318fcb5fc7ffb19e629584d0de39a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-05 21:06:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Three Great Candidates/three-great-candidates.cpp
+++ Difficulty: Easy/Three Great Candidates/three-great-candidates.cpp
@@ -4,14 +4,16 @@ class Solution {
   public:
     int maxProduct(vector<int> arr) {
         // code here
-        
-        int n = arr.size();
+        int  n = arr.size();
         
         sort(arr.begin(),arr.end());
+        int product1 = INT_MIN;
+        int product2 = INT_MIN;
         
-        int n1 = arr[n-1] * arr[n-2] * arr[n-3];
-        int n2 = arr[n-1] * arr[0] * arr[1];
+     
+        product1 = arr[n-1]*arr[n-2]*arr[n-3];
+        product2 = arr[0]*arr[1]*arr[n-1];
         
-        return max(n1,n2);
+        return max(product1,product2);
     }
 };
