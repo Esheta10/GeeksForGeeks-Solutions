a74f1e1651d1103effe1a27a2fb4ed75ea589995
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-07 10:20:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing ranges of numbers/missing-ranges-of-numbers.cpp
+++ Difficulty: Easy/Missing ranges of numbers/missing-ranges-of-numbers.cpp
@@ -2,27 +2,20 @@ class Solution {
   public:
     vector<vector<int>> missingRanges(vector<int> &arr, int lower, int upper) {
         // code here
+        vector<vector<int>> result;
+        int next_val = lower;
         
-        vector<vector<int>> res;
-        
-        if(arr.empty()){
-        res.push_back({lower, upper});
-        return res;
-    }
+        for(int num : arr){
             
-        if(lower < arr[0])
-            res.push_back({lower,arr[0]-1});
+            if(num > next_val)
+                result.push_back({next_val, num-1});
+                
+            next_val = num+1;
             
-        for(int i=0;i<arr.size()-1;i++){
-            if(arr[i+1] - arr[i] > 1){
-                res.push_back({arr[i] + 1, arr[i+1] - 1});
-            }
         }
-        
-        if(upper > arr.back())
-            res.push_back({arr.back()+1, upper});
-            
-            
-        return res;
+         if(next_val <= upper)
+                result.push_back({next_val, upper});
+                
+        return result;
     }
 };
