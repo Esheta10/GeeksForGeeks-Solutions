7ea035ae692fa7e15fbe186d3284d760b1c87095
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-18 00:23:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -1,45 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
+    int t[41];
+    int solve(int n){
+        
+        if(n < 0)
+            return 0;
+        
+        if(n == 0)
+            return 1;
+            
+        if(t[n] != -1)
+            return t[n];
+            
+        int one_step = solve(n-1);
+        int two_steps = solve(n-2);
+        
+        return t[n] = one_step + two_steps;
+    }
     int countWays(int n) {
         // your code here
-    if (n == 1) return 1;
-    if (n == 2) return 2;
-
-    int prev2 = 1, prev1 = 2, curr;
-    for (int i = 3; i <= n; i++) {
-        curr = prev1 + prev2;
-        prev2 = prev1;
-        prev1 = curr;
-    }
-    return curr;
+        memset(t, -1, sizeof(t));
+        
+        return solve(n);
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-    // taking total testcases
-    int t;
-    cin >> t;
-    while (t--) {
-        // taking stair count
-        int m;
-        cin >> m;
-        Solution ob;
-        cout << ob.countWays(m) << endl; // Print the output from our pre computed array
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
