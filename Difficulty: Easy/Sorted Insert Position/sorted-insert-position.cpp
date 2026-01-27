0ec2b5dd8a153db211d7fcea05a61c5d111d24b0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-27 16:43:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        
+        int left = 0;
+        int right = n-1;
+        int ans = n; // default value if target > all elements
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] >= k){
+                ans = mid;  // potential position
+                right = mid-1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
