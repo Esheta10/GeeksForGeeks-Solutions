8c83bd77cae1c28d44e83aa2fe2f39e01b7241e7
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-02-20 16:54:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.cpp
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.cpp
@@ -2,23 +2,22 @@ class Solution {
   public:
     int characterReplacement(string& s, int k) {
         // code here
-        unordered_map<char, int> count;
-        int left = 0;
-        int result = 0;
+        unordered_map<char, int> mp;
+        int l=0;
+        int len=0;
         int maxFreq = 0;
         
-        for(int right=0; right<s.size(); right++){
+        for(int r=0; r<s.size(); r++){
             
-            count[s[right]]++;
+            mp[s[r]]++;
+            maxFreq = max(maxFreq, mp[s[r]]);
             
-            maxFreq = max(maxFreq, count[s[right]]);
-            
-            while((right-left+1) - maxFreq > k){
-                count[s[left]]--;
-                left++;
+            while((r-l+1) - maxFreq > k){
+                mp[s[l]]--;
+                l++;
             }
-            result = max(result, right-left+1);
+            len = max(len, r-l+1);
         }
-        return result;
+        return len;
     }
 };
